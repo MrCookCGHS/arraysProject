af4a7cc600443f6c687ea1e9128f6151e92c8eab
Dave Cook <dcook@d155.org>
2020-02-17 18:57:10 -0600

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var supplies = arrayOf<String>("Ice Cream", "Cones", "Hot Fudge", "Whip Cream", "Cherries", "Nuts", "Sprinkles")
+
+    var i = 0
+
+    while (i < supplies.size) {
+        println("${supplies[i]}")
+        i++
+    }
+}
